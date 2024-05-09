7bf6adf9f1f16667938df4dcf9a783209aec29fd
pilar manavella <pilarmanavella@gmail.com>
2024-05-09 19:41:53 -0300

    intento codigo

--- Backend/services/users/users_services.go
+++ Backend/services/users/users_services.go
@@ -1,7 +1,12 @@
-package users 
+package users
+
 import "backend/domain/users"
-func Login(user string, password string) users.LoginResponse{
-	 return users.LoginResponse{
-	 	Token: "abcdef12345678",
-	 }	
+
+func Login(request users.LoginRequest) users.LoginResponse {
+
+	// Validar contra la base de datos
+
+	return users.LoginResponse{
+		Token: "abcdef123456",
+	}
 }
