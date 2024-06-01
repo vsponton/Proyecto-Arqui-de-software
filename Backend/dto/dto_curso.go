a36987f3d23e9a823f9ddb0ac5cad0aa64d6e952
pilar manavella <pilarmanavella@gmail.com>
2024-06-01 15:54:15 -0300

    modificacion DTO

--- Backend/dto/dto_curso.go
+++ Backend/dto/dto_curso.go
@@ -3,7 +3,16 @@ package dto
 
 import "time"
 
-// busqueda de cursos
+// PANTALLA DE INICIO - HOME
+
+type CourseResponse_Category struct {
+	ID           int64     `json:"id"`
+	Title        string    `json:"title"`
+	Category string `json:"category"`
+	ImageURL     string    `json:"image_url"`
+}
+
+// BUSQUEDA DE CURSOS
 
 type CourseRequest_Category struct {
 	Category string `json:"category"`
@@ -17,7 +26,6 @@ type CourseRequest_Description struct {
 	Description string `json:"description"`
 }
 
-//detalle del curso
 type CourseResponse struct {
 	ID           int64     `json:"id"`
 	Title        string    `json:"title"`
@@ -30,9 +38,41 @@ type CourseResponse struct {
 	Requirements string    `json:"requirements"`
 }
 
-//inscripcion en curso
+//DETALLE DEL CURSO
 
-//mis cursos
+type CourseResponse_Detail struct {
+	ID           int64     `json:"id"`
+	Title        string    `json:"title"`
+	Description  string    `json:"description"`
+	Category     string    `json:"category"`
+	ImageURL     string    `json:"image_url"`
+	CreationDate time.Time `json:"creation_date"`
+	Duration     int64     `json:"duration"`
+	Instructor   string    `json:"instructor"`
+	Requirements string    `json:"requirements"`
+}
+
+// INSCRIPCION EN CURSO
+
+type CourseRequest_Registration struct {
+	// sobre el usuario
+	Token string `json:"token"`
+	Email    string `json:"email"`
+	// sobre el curso
+	ID           int64     `json:"id"`
+	Title        string    `json:"title"`
+}
+
+type CourseResponse_Registration struct {
+	ID           int64     `json:"id"`
+	Title        string    `json:"title"`
+	Description  string    `json:"description"`
+	Category     string    `json:"category"`
+	ImageURL     string    `json:"image_url"`
+}
+
+
+// MIS CURSOS
 type CourseResponse struct {
 	ID           int64     `json:"id"`
 	Title        string    `json:"title"`
