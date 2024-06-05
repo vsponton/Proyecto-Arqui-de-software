c03c8948498a374a9cff67901b0c1671eae91dc2
vsponton <2146222@ucc.edu.ar>
2024-06-05 19:07:35 -0300

    changes

--- Frontend/cursos-ucc/src/index.js
+++ Frontend/cursos-ucc/src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Login from './Login';
-import Courses from './Courses'
 import reportWebVitals from './reportWebVitals';
-import Home from './Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
