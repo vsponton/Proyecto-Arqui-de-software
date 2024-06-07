b8ca57bfe323424126dfecabcf2b9fa4fc94d9f5
pilar manavella <pilarmanavella@gmail.com>
2024-06-07 19:33:37 -0300

    router

--- Backend/app/router.go
+++ Backend/app/router.go
@@ -1,14 +1,9 @@
 package app
 
 import (
-	
 	"github.com/gin-gonic/gin"
-	log "github.com/sirupsen/logrus"
 )
 
-var (
-	router *gin.Engine
-)
 func AllowCORS(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
@@ -16,21 +11,4 @@ func AllowCORS(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 	c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	c.Next()
-}
-
-func init() {
-	router = gin.Default()
-	router.Use(AllowCORS)
-	
-}
-
-func StartRoute() {
-	MapUrls(router)
-
-	log.Info("Starting server")
-	err := router.Run(":8080")
-	if err != nil {
-		return
-	} 
-
 }
